061f5a4ab1a71f037276e643d21f9283a43e7de4
KeiNJL <nikshilov32@gmail.com>
2024-05-15 17:42:05 +0300

    develop 1.0

--- Core/Src/Proc.c
+++ Core/Src/Proc.c
@@ -12,6 +12,7 @@
 #define BUT_NOT_PRESSED 0
 #define BUT_PRESS_SHORT 1
 #define BUT_PRESS_LONG 2
+#define SIZE 3
 
 
 static uint8_t state = 0;
@@ -20,6 +21,8 @@ static uint32_t startTime = 0;
 static uint32_t stopTime = 0;
 static uint32_t holdTime = 0;
 static uint32_t currentTime = 0;
+static uint32_t resetTime = 0;
+static uint8_t arr[SIZE] = {1,0,1};
 
 uint8_t getButStatus()
 {
@@ -44,30 +47,35 @@ void ProcManualButton (void)
 	if (HAL_GPIO_ReadPin(KEY_GPIO_Port, KEY_Pin) == 0)
 	{
 		startTime = currentTime;
+		resetTime = currentTime - stopTime;
 	}
 	else if (HAL_GPIO_ReadPin(KEY_GPIO_Port, KEY_Pin) == 1)
 	{
 		stopTime = currentTime;
 		holdTime = stopTime - startTime;
 		state = 1;
+		resetTime = 0;
 	}
+
 	if ((HAL_GPIO_ReadPin(KEY_GPIO_Port, KEY_Pin) == 0) && (state == 1))
 	{
 		state = 0;
-		HAL_TIM_Base_Start_IT(&htim2);
+		HAL_TIM_Base_Start_IT(&htim2);  ////////////////
 		status = getButStatus();
 		holdTime = 0;
 		state = 0;
 	}
+
+	if(resetTime >= 2000)
+	{
+		for(uint8_t i = 0; i < SIZE; i++)
+		{
+			arr[i] = 0;
+		}
+	}
 }
 
-
-void setButStatus (void)
-{
-
-}
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) ////////////////
 {
     if (htim->Instance == TIM2)
     {
